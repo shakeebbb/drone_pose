febf3dc6f68396ef40f935c803b2bc65dd5121ef
shakeebbb <36937615+shakeebbb@users.noreply.github.com>
2020-07-09 17:24:49 -0600

    Added ouster and px4 reboot scripts

---
+++ ouster_spin_up.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+nc os1-991939999414.local 7501
+set_config_param auto_start_flag 1
+reinitialize
+
---
+++ px4_reboot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./$HOME/MAVSDK/examples/reboot_autopilot/build/reboot_autopilot serial:///dev/fcu:57600
